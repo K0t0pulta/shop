4383c3a7042465f856ddd5499be444e6d7f21659
vladimir.konovalov <konovalov_va@mail.ru>
2022-05-25 18:01:59 +0300

    unfixed issue with child components

---
+++ src/data/products.js
@@ -0,0 +1,68 @@
+/* eslint-disable linebreak-style */
+export default [
+  {
+    title: 'Just Dance 2016',
+    price: 5120,
+    img: '/img/JustDance2016.jpg'
+  },
+  {
+    title: 'Star Wars The Clone Wars: Republic Heroes',
+    price: 2812,
+    img: '/img/SW.webp'
+  },
+  {
+    title: 'Sniper Elite 5',
+    price: 6520,
+    img: '/img/SniperElite.jpg'
+  },
+  {
+    title: 'Uncharted: Наследие воров. Коллекция',
+    price: 3900,
+    img: '/img/UnchartedCollection.jpg'
+  },
+  {
+    title: 'Horizon Запретный Запад',
+    price: 6099,
+    img: '/img/HorizonFW.jpg'
+  },
+  {
+    title: 'Elden Ring',
+    price: 7200,
+    img: '/img/EldenRings.jpg'
+  },
+  {
+    title: 'Ghostwire: Tokyo',
+    price: 8001,
+    img: '/img/Ghostwire.png'
+  },
+  {
+    title: 'ELEX II',
+    price: 3599,
+    img: '/img/Elex II.png'
+  },
+  {
+    title: 'Dying Light 2 Stay Human',
+    price: 5199,
+    img: '/img/DyingLight2.png'
+  },
+  {
+    title: 'SpellForce 3 Reforced',
+    price: 5499,
+    img: '/img/SpellForce3.jpg'
+  },
+  {
+    title: 'Forspoken',
+    price: 7999,
+    img: '/img/Forspoken.jpg'
+  },
+  {
+    title: 'Vampire: The Masquerade – Swansong',
+    price: 6699,
+    img: '/img/Vimpire(swansong).jpg'
+  },
+  {
+    title: 'Grand Theft Auto: The Trilogy Definitive Edition',
+    price: 3799,
+    img: '/img/GTA(trilogy).jpg'
+  }
+];
