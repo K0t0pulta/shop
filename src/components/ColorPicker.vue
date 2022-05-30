e9de21f8dc0ed9fbd5d2c21efcbf7945f240fd2e
vladimir.konovalov <konovalov_va@mail.ru>
2022-05-30 10:32:18 +0300

    ColorPicker small fix

--- src/components/ColorPicker.vue
+++ src/components/ColorPicker.vue
@@ -2,7 +2,7 @@
     <ul class="colors colors--black">
       <li class="colors__item" v-for="(color, index) in colors" :key="index">
         <label class="colors__label" :for="'color' + index">
-          <input class="colors__radio sr-only" type="radio" :value="color" :id="'color' + index" v-model="value" name="color">
+          <input class="colors__radio sr-only" type="radio" :value="color" :id="'color' + index" v-model="picked" name="color">
           <span class="colors__value" :style="'background-color: ' + color + ';'">
           </span>
         </label>
@@ -12,6 +12,11 @@
 
 <script>
 export default {
-  props: ['colors']
+  props: ['colors'],
+  data() {
+    return {
+      picked: this.colors[0]
+    };
+  }
 };
 </script>
